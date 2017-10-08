b1e550b4cc147a1fee44a31ce89d921dc46fbce6
Wojciech Reszelewski <wreszelewski@gmail.com>
2017-10-08 15:15:07 +0200

    Topics implemented

--- functions/index.js
+++ functions/index.js
@@ -13,18 +13,65 @@ exports.notificationPage = functions.https.onRequest((req, res) => {
   </html>`);
 });
 
-exports.sendNotification = functions.https.onRequest((req, res) => {
-  const payload = {
-    data: {
-      title: req.query.title,
-      body: req.query.body,
-      click_action: req.query.click_action
-    }
-  };
+exports.sendSingleNotification = functions.https.onRequest((req, res) => {
+  const payload = prepareMessage(req);
 
   return admin.messaging().sendToDevice(req.query.to, payload)
     .then(response => {
       res.status(200).send("OK");
     })
 
-})
+});
+
+exports.sendNotificationToTopic = functions.https.onRequest((req, res) => {
+  const topic = req.query.topic;
+  let payload = prepareMessage(req);
+
+    payload.data.title = topic + ': ' + payload.data.title;
+    admin.messaging().sendToTopic(topic, payload)
+    .then(function(response) {
+      res.status(200).send("OK");
+    })
+    .catch(function(error) {
+      res.status(400).send("Bad Request");
+    });
+  
+});
+
+function prepareMessage(req) {
+  return {
+    data: {
+      title: req.query.title,
+      body: req.query.body,
+      click_action: req.query.click_action
+    }
+  };
+}
+
+exports.subscribeToTopic = functions.https.onRequest((req, res) => {
+  const token = req.query.token,
+        topic = req.query.topic;
+
+  admin.messaging().subscribeToTopic(token, topic)
+     .then(function(response) {
+          res.status(200).send('OK');
+        })
+        .catch(function(error) {
+          res.status(400).send('Bad request');
+        });
+      
+});
+
+exports.unsubscribeFromTopic = functions.https.onRequest((req, res) => {
+  const token = req.query.token,
+        topic = req.query.topic;
+
+  admin.messaging().unsubscribeFromTopic(token, topic)
+     .then(function(response) {
+          res.status(200).send('OK');
+        })
+        .catch(function(error) {
+          res.status(400).send('Bad request');
+        });
+      
+});
